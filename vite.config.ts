c7900c312fecc2490a40eb2b3742ec4e5eea16ea
Maria Luiza Rodrigues <marialuiza.r.eng@gmail.com>
2024-04-23 16:44:20 -0300

    feat: adding new base url to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/ecommerce-product-page/",
+  base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
